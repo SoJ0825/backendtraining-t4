7cb50609699f2912f152188223d45f3667fc5418
Mia Tang <alice79101@gmail.com>
2022-12-07 16:14:12 +0800

    autoload & .env可行

--- app/Core/Database/DB.php
+++ app/Core/Database/DB.php
@@ -2,21 +2,29 @@
 
 namespace App\Core\Database;
 
-//------不確定以下這邊是否需要加上------
-require_once __DIR__.'/vendor/autoload.php';
-use Dotenv\Dotenv;
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+////------不確定以下這邊是否需要加上------
+//require_once __DIR__.'/vendor/autoload.php';
+//use Dotenv\Dotenv;
+//$dotenv = Dotenv::createImmutable(__DIR__);
+//$dotenv->load();
 
 class DB extends SingletonDB
 {
 
-    private $host = $_ENV['DB_HOST'];
-    private $dbUser = $_ENV['DB_USERNAME'];
-    private $dbPwd = $_ENV['DB_PASSWORD'];
-    private $dbname = $_ENV['DB_DATABASE'];
+    private $host;
+    private $dbUser;
+    private $dbPwd;
+    private $dbname;
 
-    protected function connectFunction() {
+
+    protected function connectDB(): PDO
+    {
+        $this->host = $_ENV['DB_HOST'];
+        $this->dbUser = $_ENV['DB_USERNAME'];
+        $this->dbPwd = $_ENV['DB_PASSWORD'];
+        $this->dbname = $_ENV['DB_DATABASE'];
+        var_dump($_ENV['DB_DATABASE']);
+
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
         $pdo = new PDO($dsn, $this->dbUser, $this->dbPwd);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
