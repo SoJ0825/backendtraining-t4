887b109c65a41a43542fd87801a4817d557697e5
Lucas <tsaoyuan@gmail.com>
2022-12-07 15:45:57 +0800

    feat: createImmutable() method
    - why no use super-globals, but can getenv()?
    - `getenv('DEMO')` 取的值是 第 10 行 的關係，因 `putenv('DEMO=666666')`
    - 實則 putenv() 並沒有變更 .env 內的 DEMO's value， DEMO's value still `abc111`

--- demo.php
+++ demo.php
@@ -1,10 +1,13 @@
 <?php
   require './vendor/autoload.php';
 
-  $dotenv =  Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+// $dotenv =  Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+  $dotenv =  Dotenv\Dotenv::createImmutable(__DIR__); 
   $dotenv->load();
+  $demo = $_ENV['DEMO'];
 
-  echo "hi ".getenv('DEMO')." bro.".PHP_EOL;
+  echo "hi ". $demo ." bro.".PHP_EOL;
   putenv('DEMO=666666');
   echo "hi ".getenv('DEMO')." bro.".PHP_EOL;
+  echo "hi ". $demo ." bro.".PHP_EOL;
   
