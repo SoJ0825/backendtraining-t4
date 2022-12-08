80877bba0710a341e6d156e2cccd26eae227e0c0
Lucas <tsaoyuan@gmail.com>
2022-12-08 14:44:14 +0800

    feat: Use Dotenv and Opis link DB

--- demo.php
+++ demo.php
@@ -3,28 +3,14 @@
   use Opis\Database\Connection;
   use Opis\Database\Database;
 
-// $dotenv =  Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
-  // $dotenv =  Dotenv\Dotenv::createImmutable(__DIR__); 
-  // $dotenv->load();
+  $dotenv =  Dotenv\Dotenv::createImmutable(__DIR__); 
+  $dotenv->load();
   // $demo = $_ENV['DEMO'];
 
-  // echo "hi ". $demo ." bro.".PHP_EOL;
-  // putenv('DEMO=666666');
-  // echo "hi ".getenv('DEMO')." bro.".PHP_EOL;
-  // echo "hi ". $demo ." bro.".PHP_EOL;
+  $dsn = 'mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_DATABASE'];
+  $user = $_ENV['DB_USERNAME'];
+  $password = $_ENV['DB_PASSWORD'];
 
-  $dsn = 'mysql:host=localhost;dbname=blog';
-  $user = 'luna';
-  $password = '0000';
-
-  // try{
-  //   $conn = new PDO($dsn, $user, $password);
-  //   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //   echo "Connected successfully";
-  // }catch(PDOException $e) {
-  //   echo "Connection failed: " . $e->getMessage();
-  // }
-  
   try {
     $connection = new Connection($dsn, $user, $password);
     $connection->getPDO(); // 這裡才會真的透過 PDO 連線
