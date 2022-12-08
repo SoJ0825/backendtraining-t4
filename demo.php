d0e1758213ab4474c331d26fe59a0b6f5f28c2db
Lucas <tsaoyuan@gmail.com>
2022-12-08 14:11:45 +0800

    fix issue1: OPIS Connection method
    - IDE 顯示 class undefined type error 是 VScode 套件造成, 不影響操作
    - 無論密碼對錯，都只顯示 Connection Successful
      的字，無法正常顯示錯誤。--> Opis\Database\Connection 找其內部 source
    code 本身只是先準備好連線資訊，實際上要透過 method getPDO() 才會正式連
    線資料庫，也才會判斷連線資訊是否正確
    - 完成與 database 連線; 連線資訊錯誤, 回報錯誤訊息

--- demo.php
+++ demo.php
@@ -1,36 +1,36 @@
 <?php
   require './vendor/autoload.php';
   use Opis\Database\Connection;
-  // use Opis\Database\Database;
+  use Opis\Database\Database;
 
 // $dotenv =  Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
-  $dotenv =  Dotenv\Dotenv::createImmutable(__DIR__); 
-  $dotenv->load();
-  $demo = $_ENV['DEMO'];
+  // $dotenv =  Dotenv\Dotenv::createImmutable(__DIR__); 
+  // $dotenv->load();
+  // $demo = $_ENV['DEMO'];
 
-  echo "hi ". $demo ." bro.".PHP_EOL;
-  putenv('DEMO=666666');
-  echo "hi ".getenv('DEMO')." bro.".PHP_EOL;
-  echo "hi ". $demo ." bro.".PHP_EOL;
+  // echo "hi ". $demo ." bro.".PHP_EOL;
+  // putenv('DEMO=666666');
+  // echo "hi ".getenv('DEMO')." bro.".PHP_EOL;
+  // echo "hi ". $demo ." bro.".PHP_EOL;
 
   $dsn = 'mysql:host=localhost;dbname=blog';
   $user = 'luna';
   $password = '0000';
 
-  try{
-    $conn = new PDO($dsn, $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-  }catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
-  
   // try{
-  //   $conn = new Connection($dsn, $user, $password);
-  //   $conn->option(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-  //   $conn->option(PDO::ATTR_STRINGIFY_FETCHES, false);
+  //   $conn = new PDO($dsn, $user, $password);
+  //   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //   echo "Connected successfully";
-  // }catch(PDOException $e){
+  // }catch(PDOException $e) {
   //   echo "Connection failed: " . $e->getMessage();
   // }
+  
+  try {
+    $connection = new Connection($dsn, $user, $password);
+    $connection->getPDO(); // 這裡才會真的透過 PDO 連線
+    $db = new Database($connection);
+    echo "Connected successfully!";
+  } catch (Exception $exception) {
+      echo $exception->getMessage();  
+  }
   
