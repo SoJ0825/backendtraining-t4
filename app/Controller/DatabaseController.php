03a730f6faf8942a5a077080d64d703f18735579
Mia Tang <alice79101@gmail.com>
2022-12-09 16:04:14 +0800

    創建兩個表格

--- app/Controller/DatabaseController.php
+++ app/Controller/DatabaseController.php
@@ -12,22 +12,38 @@ class DatabaseController implements RainfallSchema, CollectData
 {
     protected $db;
     public function __construct($pdo){
+        //DB連線
         $connection = Connection::fromPDO($pdo);
         $db = new Database($connection);
 //        echo "成功？";
 //        $result = $db->from('users')->select()->all();
 //        print_r($result);
-        return $db;
+        $this->db = $db;
+        $this->createRainfallsTable();
+        $this->createDistrictsTable();
     }
 
     public function createRainfallsTable(){
-        $db->schema()->create('test',function (CreateTable $table) {
-
+        var_dump($this->db);
+        //drop table
+        $this->db->schema()->drop('RainfallsTable');
+        //重新建立table
+        $this->db->schema()->create('RainfallsTable',function (CreateTable $table) {
+            $table->integer('index')->primary()->unsigned()->autoincrement();
+            $table->string('區域', 30);
+            $table->dateTime('日期');
+            $table->decimal('雨量', 3, 1);
         });
     }
 
     public function createDistrictsTable(){
-
+        //drop table
+//        $this->db->schema()->drop('DistrictsTable');
+        //重新建立table
+        $this->db->schema()->create('DistrictsTable', function (CreateTable $table) {
+            $table->integer('districtNum')->primary()->autoincrement();
+            $table->string('區域名', 30);
+        });
     }
 
     public function importData(){
