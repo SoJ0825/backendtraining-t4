49454cd573870c2ca75b5324855633ef42031aea
Lucas <tsaoyuan@gmail.com>
2022-12-12 17:30:02 +0800

    feat: Get list of all filenames
    - use pathinfo
    - glob() — Find pathnames matching a pattern

--- demo.php
+++ demo.php
@@ -14,13 +14,28 @@
   try {
     $connection = new Connection($dsn, $user, $password);
     $connection->getPDO(); // 這裡才會真的透過 PDO 連線
-    echo "Connected successfully!";
+    echo "Connected successfully!".PHP_EOL;
   } catch (Exception $exception) {
       echo $exception->getMessage();  
   }
 
   $db = new Database($connection);
-  $result = $db->from('users')
-             ->select()
-             ->all();
-  print_r($result);
+  // $result = $db->from('users')
+            //  ->select()
+            //  ->all();
+  // print_r($result);
+
+  $path = '/var/www/html/weather/backendtraining-t4/rainfallData/C0X270_柳營.json';
+  $fileName = pathinfo($path, PATHINFO_FILENAME);
+  $baseName = pathinfo($path, PATHINFO_BASENAME);
+
+  echo "filename: $fileName, basename: $baseName" .PHP_EOL;
+  $splice = substr($fileName,'7');
+  echo $splice.PHP_EOL;
+
+  $pathJson = '/var/www/html/weather/backendtraining-t4/rainfallData/*.*';
+  foreach(glob($pathJson) as $jsonFileName){
+   $fileName = pathinfo($jsonFileName, PATHINFO_FILENAME);   
+   $splice = substr($fileName,'7');
+   echo $splice.PHP_EOL;
+}
