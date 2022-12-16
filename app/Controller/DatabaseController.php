075c00572ea11e988d7c38a7928ec1340b3eb8f7
Lucas <tsaoyuan@gmail.com>
2022-12-16 16:53:49 +0800

    feat: Draft controller importData()
    - Create Rainfalls and Districts tables sucess

--- app/Controller/DatabaseController.php
+++ app/Controller/DatabaseController.php
@@ -21,10 +21,10 @@ class DatabaseController implements RainfallSchema, CollectData
 // Property 
 // index.php line 12 用 $pdo 作為連線的物件
 // 無須使用 $conn, $dsn, $user, $pwd
-    private $pdo, $db;
+    private $pdo, $db, $schema;
 // table name
-    private $rainfallsTableName;
-    private $districtsTableName;
+    private $rainfallsTableName = 'rainfall';
+    private $districtsTableName = 'districts';
 // Methods
 // RainfallSchema
     public function __construct($pdo){
@@ -33,25 +33,62 @@ class DatabaseController implements RainfallSchema, CollectData
         $this->pdo = Connection::fromPDO($pdo);
         // 建立 $db 物件
         $this->db = new Database($this->pdo);
-        $schema = $this->db->schema();
+        $this->schema = $this->db->schema();
     }
 
     public function createRainfallsTable(){
-        
+        $tables = $this->rainfallsTableName;
+        $this->schema->drop($tables);
+        $this->schema->create($tables, function(CreateTable $table){
+        $table->integer('id')->primary()->autoincrement();
+        $table->string('name', 8);
+        $table->dateTime('datetime');
+        $table->float('rainfall');
+        });
+       echo "Catch  Create Rain table" .PHP_EOL;
     }
 
     public function createDistrictsTable(){
+        $tables = $this->districtsTableName;
+        $this->schema->drop($tables);
+        $this->schema->create($tables, function(CreateTable $table){
+            $table->integer('id')->primary()->autoincrement();
+            $table->string('name', 8);
+        });
+       echo "Catch  Create Districts table" .PHP_EOL;
     }
 
     public function importData(){
+      $this->createRainfallsTable(); 
+      $this->createDistrictsTable();
+     // Check databases have any tables?   
+       // Databases have no tables
+         // Create rainfallTable and districtTable, then import data
+       // Databases have tables
+         // 1.
+         // have rainfallTable and districtTable, clear two table's row data, then import data 
+         // 2.
+         // have rainfallTable, clear table's all row data, then import data 
+         // Create districtTable, then import data
+         // 3.
+         // have districtTable, clear table's all row data, then import data
+         // Create rainfallTable, then import data
+         
+
     }
 // CollectData
     public function showDistricts(): array{
+        $result = ['Creating...'];
+        return $result;
     }
 
     public function sumByYear($district = null): array{
+        $result = ['Creating...'];
+        return $result;
     }
 
     public function sumByMonth($district = null): array{
+        $result = ['Creating...'];
+        return $result;
     }
 }
