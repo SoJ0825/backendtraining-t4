8d6572ae28c63ae021e016aa0dd6fef5edce3fb1
Lucas <tsaoyuan@gmail.com>
2022-12-17 18:29:45 +0800

    feat: Connection by using the instance of PDO
    - var_dump of $this->pdo is object(PDO)#21
    - `$this->db = new Database($this->pdo)` error msg: Uncaught TypeError: Argument 1 passed to Opis\Database\Database::__construct()
      - `$this->db` error of reason: 拿到的引數是 PDO 的實體，也就是 Fatal error: Uncaught TypeError: Argument 1 passed..等錯誤
      - new Database($this->pdo)的引數必須是 an instance of Opis\Database\Connection

--- app/Controller/DatabaseController.php
+++ app/Controller/DatabaseController.php
@@ -31,14 +31,13 @@ class DatabaseController implements RainfallSchema, CollectData
         // 建立連線前置作業
         echo PHP_EOL.'var_dump of $pdo  is ';var_dump($pdo);
         echo PHP_EOL.'var_dump of $this->pdo  is ';var_dump($this->pdo);
-        // connection by using the fromPDO static method 
-        $this->pdo = Connection::fromPDO($pdo);
+        // connection by using the instance of PDO 
+        $this->pdo = $pdo;
+        echo PHP_EOL.'var_dump of $this->pdo is ';var_dump($this->pdo);
         
-        // 建立 $db 物件
         $this->db = new Database($this->pdo);
         // what's in the $this->db?
-        echo PHP_EOL.'$this->db var_dump is ';var_dump($this->db);
-        $this->schema = $this->db->schema();
+        echo PHP_EOL.'var_dump of $this->db  is ';var_dump($this->db);
     }
 
     public function createRainfallsTable(){
